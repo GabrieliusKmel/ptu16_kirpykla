f3c67a119ee223c5ac5e46107d65b2c686bb342a
GabrieliusKmel <5132zzm@gmail.com>
2023-10-16 14:50:27 +0300

    adminas

--- gp_kirpykla/kirpykla/admin.py
+++ gp_kirpykla/kirpykla/admin.py
@@ -1,3 +1,33 @@
 from django.contrib import admin
+from . import models
 
-# Register your models here.
+
+class ServiceOrderAdmin(admin.ModelAdmin):
+    list_display = ("barber", "service_name", "customer", "service_time", "created_at")
+    list_filter = ("barber", "service")
+    list_display_links = ("barber", )
+    search_fields = ("serviceorder__barber", "serviceorder__service")
+    readonly_fields = ("id", )
+
+    def service_name(self, obj):
+        return obj.service.name
+    service_name.short_description = "Service Name"
+
+class ServiceAdmin(admin.ModelAdmin):
+    list_display = ("name", "price", "duration", "about")
+    list_filter = ("name", "price")
+    list_display_links = ("name", )
+    search_fields = ("service__name", )
+    readonly_fields = ("id", )
+    
+
+@admin.register(models.BarberReview)
+class BarberReviewAdmin(admin.ModelAdmin):
+    list_display = ('barber', 'reviewer', 'created_at')
+    list_display_links = ('created_at', )
+
+
+admin.site.register(models.ServiceOrder, ServiceOrderAdmin)
+admin.site.register(models.Service, ServiceAdmin)
+
+
--- gp_kirpykla/kirpykla/models.py
+++ gp_kirpykla/kirpykla/models.py
@@ -1,3 +1,104 @@
 from django.db import models
+from django.utils.translation import gettext_lazy as _
+from django.urls import reverse
+from django.contrib.auth import get_user_model
+from tinymce.models import HTMLField
+from datetime import timedelta
 
-# Create your models here.
+
+User = get_user_model()
+
+
+class Service(models.Model):
+    name = models.CharField(_("name"), max_length=50)
+    price = models.DecimalField(_("price"), max_digits=18, decimal_places=2)
+    duration = models.DurationField(_("duration"), default=timedelta(minutes=30))
+    about = HTMLField(_("about"), max_length=10000, default='', blank=True)
+    
+    class Meta:
+        verbose_name = _("service")
+        verbose_name_plural = _("services")
+
+    def __str__(self):
+        return f"{self.name} {self.price} {self.duration} {self.about}"
+
+    def get_absolute_url(self):
+        return reverse("service_detail", kwargs={"pk": self.pk})
+    
+SERVICEORDER_STATUS = (
+    (0, _("confirmed")),
+    (1, _("completed")),
+    (2, _("cancelled")),
+)
+
+
+class ServiceOrder(models.Model):
+    barber = models.ForeignKey(
+        User, 
+        verbose_name=_("barber"), 
+        on_delete=models.CASCADE,
+        related_name="jobs",
+        null=True,
+        blank=True,
+    )
+    service = models.ForeignKey(
+        Service, 
+        verbose_name=_("service"), 
+        on_delete=models.CASCADE
+    )
+    customer = models.ForeignKey(
+        User, 
+        verbose_name=_("customer"), 
+        on_delete=models.CASCADE,
+        related_name="service_orders",
+        )
+    status = models.PositiveSmallIntegerField(
+        _("status"),
+        choices=SERVICEORDER_STATUS, 
+        default=0,
+    )
+    service_time = models.DateTimeField(_("service_time"), null=True, blank=True)
+    created_at = models.DateTimeField(_("created_at"), auto_now=False, auto_now_add=True)
+
+    class Meta:
+        verbose_name = _("service order")
+        verbose_name_plural = _("service orders")
+
+    def __str__(self):
+        return f"{self.barber} {self.service} {self.customer}"
+
+    def get_absolute_url(self):
+        return reverse("serviceorder_detail", kwargs={"pk": self.pk})
+
+
+class BarberReview(models.Model):
+    barber = models.ForeignKey(
+        User, 
+        verbose_name=_("barber"), 
+        on_delete=models.CASCADE,
+        related_name="customer_reviews",
+    )
+    reviewer = models.ForeignKey(
+        User, 
+        verbose_name=_("reviewer"), 
+        on_delete=models.CASCADE,
+        related_name='barber_reviews',
+    )
+    content = models.TextField(_("content"), max_length=4000)
+    created_at = models.DateTimeField(
+        _("created at"), 
+        auto_now_add=True, 
+        db_index=True
+    )
+
+    class Meta:
+        verbose_name = _("barber review")
+        verbose_name_plural = _("barber reviews")
+
+    def __str__(self):
+        return f"{self.barber} review by {self.reviewer}"
+
+    def get_absolute_url(self):
+        return reverse("barberreview_detail", kwargs={"pk": self.pk})
+
+
