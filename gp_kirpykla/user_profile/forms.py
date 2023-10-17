93d6c2dd722829b41b2a2004ea29e684bfdf9a24
povilastsk <povilastiskus@gmail.com>
2023-10-17 09:43:43 +0300

    user profile, etc.

---
+++ gp_kirpykla/user_profile/forms.py
@@ -0,0 +1,15 @@
+from django import forms
+from django.contrib.auth import get_user_model
+from . import models
+
+User = get_user_model()
+
+
+class UserUpdateForm(forms.ModelForm):
+    email = forms.EmailField()
+
+    class Meta:
+        model = User
+        fields = ['username', 'email', 'first_name', 'last_name']
+
+
