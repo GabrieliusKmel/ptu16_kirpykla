0dccd22eec40ea76798102bc026448ae28941a51
povilastsk <povilastiskus@gmail.com>
2023-10-18 20:41:24 +0300

    services link with booking. partial autofill

--- gp_kirpykla/kirpykla/urls.py
+++ gp_kirpykla/kirpykla/urls.py
@@ -6,5 +6,4 @@ urlpatterns = [
     path('about_us/', views.about_us, name="about_us"),
     path('services/', views.ServiceListView.as_view(), name='service_list'),
     path('book-service/', views.book_service, name='book_service'),
-    path('booking-success/', views.booking_success, name='booking_success'),
 ]
